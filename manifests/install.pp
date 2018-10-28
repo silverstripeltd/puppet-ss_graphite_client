89f90e70c1a069a8f1992ce227b90f33102a2cd5
John <john@silverstripe.com>
2018-10-29 11:42:58 +1300

    Run relay on different port to ensure no conflict where Carbon also runs (Graphite server)

--- manifests/install.pp
+++ manifests/install.pp
@@ -52,64 +52,75 @@ class ss_graphite_client::install inherits ss_graphite_client {
       source  => "puppet:///modules/ss_graphite_client/graphite_functions.php",
   }
 
-  # Metrics always sent to localhost. If graphite_server is external, relay will forward appropriately
-  file { "/etc/cron.d/graphite-scripts":
-    ensure  => present,
-    owner   => root,
-    group   => root,
-    mode    => 755,
-    content => "* * * * *       root    run-parts /opt/graphite/scripts --arg=localhost --arg=2003>/dev/null 2>&1\n",
-    require => File['/opt/graphite/scripts'],
-  }
+  # Setup and install carbon-c-relay
+  if $lsbdistcodename == 'jessie' {
 
-  file { "/etc/cron.d/graphite-scripts-daily":
-    ensure  => present,
-    owner   => root,
-    group   => root,
-    mode    => 0755,
-    content => "34 4 * * *       root    run-parts /opt/graphite/scripts-daily --arg=localhost --arg=2003 >/dev/null 2>&1\n",
-    require => File['/opt/graphite/scripts-daily'],
-  }
-
-  # Only set up relay if server not localhost
-  if $graphite_server != 'localhost' {
-    if $lsbdistcodename == 'jessie' {
-      file { "/usr/src/carbon-c-relay_2.5-1~bpo8+1_amd64.deb":
-        ensure => present,
-        owner => root,
-        group => root,
-        mode => 0644,
-        source => 'puppet:///modules/ss_graphite_client/carbon-c-relay_2.5-1~bpo8+1_amd64.deb',
-      }
-      package { "carbon-c-relay":
-        ensure => latest,
-        provider => dpkg,
-        source => '/usr/src/carbon-c-relay_2.5-1~bpo8+1_amd64.deb',
-        require => File['/usr/src/carbon-c-relay_2.5-1~bpo8+1_amd64.deb'],
-      }
-    } else {
-      package { "carbon-c-relay":
-        ensure => latest,
-      }
-    }
-
-    file { "/etc/carbon-c-relay.conf":
+    file { "/usr/src/carbon-c-relay_2.5-1~bpo8+1_amd64.deb":
       ensure => present,
       owner => root,
       group => root,
       mode => 0644,
-      content => template('ss_graphite_client/carbon-c-relay.conf.erb'),
-      require => Package['carbon-c-relay'],
-      notify => Service['carbon-c-relay'],
+      source => 'puppet:///modules/ss_graphite_client/carbon-c-relay_2.5-1~bpo8+1_amd64.deb',
+    }
+    package { "carbon-c-relay":
+      ensure => latest,
+      provider => dpkg,
+      source => '/usr/src/carbon-c-relay_2.5-1~bpo8+1_amd64.deb',
+      require => File['/usr/src/carbon-c-relay_2.5-1~bpo8+1_amd64.deb'],
     }
 
-    service { "carbon-c-relay":
-      enable => true,
-      ensure => running,
-      hasstatus => true,
-      hasrestart => true,
+  } else {
+
+    package { "carbon-c-relay":
+      ensure => latest,
     }
 
+  }
+
+  file { "/etc/carbon-c-relay.conf":
+    ensure => present,
+    owner => root,
+    group => root,
+    mode => 0644,
+    content => template('ss_graphite_client/carbon-c-relay.conf.erb'),
+    require => Package['carbon-c-relay'],
+    notify => Service['carbon-c-relay'],
+  }
+
+  file { "/etc/default/carbon-c-relay":
+    ensure => present,
+    owner => root,
+    group => root,
+    mode => 0644,
+    content => template('ss_graphite_client/carbon-c-relay.defaults.erb'),
+    require => Package['carbon-c-relay'],
+    notify => Service['carbon-c-relay'],
+  }
+
+  service { "carbon-c-relay":
+    enable => true,
+    ensure => running,
+    hasstatus => true,
+    hasrestart => true,
+  }
+
+  # Metrics always sent to relay @ localhost on port 2013.
+  file { "/etc/cron.d/graphite-scripts":
+  ensure  => present,
+  owner   => root,
+  group   => root,
+  mode    => 755,
+  content => "* * * * *       root    run-parts /opt/graphite/scripts --arg=localhost --arg=2013 >/dev/null 2>&1\n",
+  require => File['/opt/graphite/scripts'],
+  }
+
+  file { "/etc/cron.d/graphite-scripts-daily":
+  ensure  => present,
+  owner   => root,
+  group   => root,
+  mode    => 0755,
+  content => "34 4 * * *       root    run-parts /opt/graphite/scripts-daily --arg=localhost --arg=2013 >/dev/null 2>&1\n",
+  require => File['/opt/graphite/scripts-daily'],
   }
 
 }
