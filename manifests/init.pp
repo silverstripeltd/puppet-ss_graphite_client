cfad72f568d9209d6d67c5c222e40ebf7a42c319
Mateusz Uzdowski <mateusz@silverstripe.com>
2020-08-11 14:04:55 +1200

    Revert "Add .editorconfig and reformat. Format python to PEP-8."
    This reverts commit 2348755ca3c1ce15e9641b25781714424081127b.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,14 @@
 class ss_graphite_client (
-  $graphite_server = 'localhost',
-  $graphite_port = '2003',
-  $relay_port = '2013',
-  $use_carbon_c_relay = true,
-  $configure_tcp_queue_size_metric = true,
+	$graphite_server = "localhost",
+	$graphite_port = "2003",
+	$relay_port = "2013",
+	$use_carbon_c_relay = true,
+	$configure_tcp_queue_size_metric = true,
 ) {
 
-  class { 'ss_graphite_client::install': }
+	class { 'ss_graphite_client::install': }
 
-  if $configure_tcp_queue_size_metric == true {
-    create_resources(ss_graphite_client::tcp_queue_size, hiera('ss_graphite_client::tcp_queue_size', {}))
-  }
+	if $configure_tcp_queue_size_metric == true {
+		create_resources(ss_graphite_client::tcp_queue_size, hiera('ss_graphite_client::tcp_queue_size', {}))
+	}
 }
